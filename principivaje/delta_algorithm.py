c91dbd2fe678276078613d4ef531de0f16a747d5
Marko Burjek <s0mebody.slo@gmail.com>
2012-11-09 16:11:40 +0100

    Change delta algorithom to class

--- principivaje/delta_algorithm.py
+++ principivaje/delta_algorithm.py
@@ -23,52 +23,67 @@ def calc_ep(p, d, y, text_output):
 
 
 
+class DeltaLearning(object):
 
-def delta(w, x, d, eta, text_output=[], epoch_learning=True):
-    """Delta learning algorithm
+    def __init__(self, w, x, d, eta, text_output=[], epoch_learning=True,
+                 neuron_type = 'logistic'):
+        """Delta learning algorithm
 
-    w - numpy array of weights
-    x - input values
-    d - right values
-    eta - learning rate
-    """
-    log_neuron = LogisticNeuron(w, text_output)
-    deltas_w = []
-    e_ps = []
-    table = []
-    header = ["p"]
-    number_of_weights = len(w)
-    number_of_input_values = number_of_weights - 1# - bias
-    for i in range(1, number_of_input_values):
-        header.append("\(x_%d^{(p)}\)" % i)
-    header.extend(["\(v^{(p)}\)","\(y^{(p)}\)", "\(d^{(p)}\)"])
-    for i in range(0, number_of_weights):
-        header.append("\(\delta_%d^{(p)}\)" % i)
-    for i in range(0, number_of_weights):
-        header.append("\(\delta_%d\)" % i)
-    header.append("\(e^{(p)}\)")
-    table.append(header)
-    #for head in chain(["p"],
-            #header.append("\(x_%d^{(p)}\)" % i)
+        w - numpy array of weights
+        x - numpy array input values
+        d - true values
+        eta - learning rate
+        """
+        self.w = w
+        self.x = x
+        self.d = d
+        self.eta = eta
+        self.text_output = text_output
+        self.epoch_learning = epoch_learning
+        self.neurons = {'logistic':LogisticNeuron}
+        if neuron_type in self.neurons:
+            self.neuron = self.neurons[neuron_type](w, text_output)
+        else:
+            raise Exception("Neuron type: %s doesn't exist!" % neuron_type)
+        self.table = []
+        header = ["p"]
+        number_of_weights = len(w)
+        number_of_input_values = number_of_weights - 1# - bias
+        for i in range(1, number_of_input_values):
+            header.append("\(x_%d^{(p)}\)" % i)
+        header.extend(["\(v^{(p)}\)","\(y^{(p)}\)", "\(d^{(p)}\)"])
+        for i in range(0, number_of_weights):
+            header.append("\(\delta_%d^{(p)}\)" % i)
+        for i in range(0, number_of_weights):
+            header.append("\(\delta_%d\)" % i)
+        header.append("\(e^{(p)}\)")
+        self.table.append(header)
+
+    def run(self):
+        deltas_w = []
+        e_ps = []
+        #for head in chain(["p"],
+                #header.append("\(x_%d^{(p)}\)" % i)
 # for each learning sample
-    for p, x_sample, d_sample in zip(range(1, len(x) + 1), x, d):
-        print x_sample, d_sample
+        for p, x_sample, d_sample in zip(range(1, len(self.x) + 1),
+                                         self.x, self.d):
+            print x_sample, d_sample
 # calculate activation v and output y
-        y = log_neuron.get_output(x_sample)
-        print y
-        delta_w = adjust_weight(p, x_sample, d_sample, eta, y, text_output)
-        deltas_w.append(delta_w)
-        print delta_w
-        e_p = calc_ep(p, d_sample, y, text_output)
-        e_ps.append(e_p)
-        print e_p
-    text = "$$E = \\frac{1}{2}(e^{(1)}+e^{(2)})$$"
-    text_output.append(text)
-    E = 1/2.0 * sum(e_ps)
-    text = "$$E = \\frac{1}{2}(%.3g + %.3g) = %.3g" % (e_ps[0], e_ps[1], E)
-    text_output.append(text)
-    print sum(deltas_w)
-    return table
+            y = self.neuron.get_output(x_sample)
+            print y
+            delta_w = adjust_weight(p, x_sample, d_sample, self.eta,
+                                    y, self.text_output)
+            deltas_w.append(delta_w)
+            print delta_w
+            e_p = calc_ep(p, d_sample, y, self.text_output)
+            e_ps.append(e_p)
+            print e_p
+        text = "$$E = \\frac{1}{2}(e^{(1)}+e^{(2)})$$"
+        text_output.append(text)
+        E = 1 / 2.0 * sum(e_ps)
+        text = "$$E = \\frac{1}{2}(%.3g + %.3g) = %.3g" % (e_ps[0], e_ps[1], E)
+        text_output.append(text)
+        print sum(deltas_w)
 
         #break
 
@@ -88,7 +103,8 @@ if __name__ == "__main__":
     bias = np.zeros((x.shape[0], 1)) - 1
     x = np.hstack((bias, x))
     text_output = []
-    print delta(w, x, d, eta, text_output, epoch_learning=True)
+    delta = DeltaLearning(w, x, d, eta, text_output, epoch_learning=True)
+    delta.run()
     for text in text_output:
         print text
     print
